78578f61c940755e978b159292d122b148c9dc1b
Halkano Malise <kudezx@gmail.com>
2023-08-23 20:57:25 +0300

    updated

--- src/main/java/com/systechafrica/variables/Variables.java
+++ src/main/java/com/systechafrica/variables/Variables.java
@@ -11,6 +11,25 @@ public class Variables {
     //? integer variable
     int score = 0;
     LOGGER.info("Score for variable value is " + score);
+
+    //?constant variable
+    final double PI = 3.14159;
+      LOGGER.info("The Constant variable of PI is " + PI);
+      
+      //?MAX AND MIN INT VALUE
+      LOGGER.info("Integer minimum " + Integer.MIN_VALUE);
+      LOGGER.info("Integer maximum " + Integer.MAX_VALUE);
+
+      //?MAX AND MIN SHORT VALUE
+      LOGGER.info("Short minimum " + Short.MIN_VALUE);
+      LOGGER.info("Short maximum " + Short.MAX_VALUE);
+
+      //?MAX AND MIN LONG VALUE
+      LOGGER.info("Long minimum " + Long.MIN_VALUE);
+      LOGGER.info("Long maximum " + Long.MAX_VALUE);
+
+
+
   }
 
 }
