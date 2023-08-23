476f96b51e575964b2ff7235e14255a13f2795ef
Halkano Malise <kudezx@gmail.com>
2023-08-23 14:44:42 +0300

    junit-test-revision

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -1,13 +1,25 @@
 package com.systechafrica;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    //?Additional
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    //?Subtraction
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    //?Boolean
+    public boolean positiveNumber(int a){
+        return a >= 0;
     }
 }
--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -1,20 +1,45 @@
 package com.systechafrica;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+    //? Reference => App
+    App app = new App();
+    
+    //?checks for additional properties
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void add(){
+        //when 
+        int result = app.add(2, 3);
+        //then
+        int expected = 5;
+        //verify that result = expected
+        Assertions.assertEquals(expected,result, "The sum of 2+3 should be 5");
+    }
+
+    //?checks for subtractions 
+    @Test
+    void sub(){
+        //calculating the result
+        int result = app.sub(3, 2);
+
+        //expectation after the results
+        int expected = 1;
+
+        Assertions.assertEquals(expected, result, "The Sub of 3-2 is 1");
+    }
+
+    //?Checks for a boolean answer
+    @Test
+    void positiveNumber(){
+        //checking the result
+        boolean result = app.positiveNumber(5);
+
+        //expectation
+        boolean expected = true;
+
+        Assertions.assertEquals(expected, result, "a is a positive number");
     }
 }
