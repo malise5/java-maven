d38625bb27088766c8b479c9426bd00dce90f94e
Halkano Malise <kudezx@gmail.com>
2023-08-24 14:04:29 +0300

    updated

--- src/main/java/com/systechafrica/operators/Operators.java
+++ src/main/java/com/systechafrica/operators/Operators.java
@@ -23,22 +23,73 @@ public class Operators {
         LOGGER.info("Age product: " + ageProduct);
 
         // division operator
-        //? division between integers result to an integer value otherwise double value
+        // ? division between integers result to an integer value otherwise double value
         int divisionResult = studentOneAge / studentTwoAge;
-        LOGGER.info("Division: " +divisionResult);
+        LOGGER.info("Division: " + divisionResult);
 
-        //! To ensure correct values with precision
-        double divisionResultInDouble = (double)studentOneAge / studentTwoAge;
-        LOGGER.info("Division with precision: " +divisionResultInDouble);
+        // ! To ensure correct values with precision
+        double divisionResultInDouble = (double) studentOneAge / studentTwoAge;
+        LOGGER.info("Division with precision: " + divisionResultInDouble);
 
         // modulus operator => remainder of division result
         int remainder = studentOneAge % studentTwoAge;
-        LOGGER.info("Remainder: " +remainder);
+        LOGGER.info("Remainder: " + remainder);
 
+    }
+
+    public void assignmentOperators() {
+        // simple assignment operator
+        int quantity = 35;
+
+        // addition assignment operator
+        quantity += 10; // > quantity = quantity + 10;
+        LOGGER.info("Quantity: " + quantity);
+
+        // multiplication assignment operator
+        quantity *= 100; // > quantity = quantity * 100;
+        LOGGER.info("Quantity: " + quantity);
+        // multiplication assignment operator
+        quantity /= 20; // > quantity = quantity / 20;
+        LOGGER.info("Quantity: " + quantity);
+    }
+
+    public void comparisonOperators() {
+        int studentOneAge = 20;
+        int studentTwoAge = 25;
+
+        // equality operator
+        boolean isStudentOneAgeEqualToStudentTwoAge = studentOneAge == studentTwoAge;
+        LOGGER.info("isStudentOneAgeEqualToStudentTwoAge: " + isStudentOneAgeEqualToStudentTwoAge);
+
+        // Inequality operator
+        boolean isStudentOneAgeNotEqualToStudentTwoAge = studentOneAge != studentTwoAge;
+        LOGGER.info("isStudentOneAgeNotEqualToStudentTwoAge: " + isStudentOneAgeNotEqualToStudentTwoAge);
+
+        // less than operator
+        boolean isStudentOneAgeLessThanStudentTwoAge = studentOneAge < studentTwoAge;
+        LOGGER.info("isStudentOneAgeLessThanStudentTwoAge: " + isStudentOneAgeLessThanStudentTwoAge);
+
+        // greater than operator
+        boolean isStudentOneAgeGreaterThanStudentTwoAge = studentOneAge > studentTwoAge;
+        LOGGER.info("isStudentOneAgeGreaterThanStudentTwoAge: " + isStudentOneAgeGreaterThanStudentTwoAge);
+
+        // TODO: Do one for less than or equal to operator
+        //code here
+        boolean isStudentOneAgeLessThanOrEqualToStudentTwoAge = studentOneAge <= studentTwoAge;
+        LOGGER.info("isStudentOneAgeLessThanOrEqualToStudentTwoAge: " + isStudentOneAgeLessThanOrEqualToStudentTwoAge);
+
+
+        // TODO: Do one for greater than or equal to operator
+        //code here
+        boolean isStudentOneAgeGreaterThanOrEqualToStudentTwoAge = studentOneAge >= studentTwoAge;
+        LOGGER.info("isStudentOneAgeGreaterThanOrEqualToStudentTwoAge: " + isStudentOneAgeGreaterThanOrEqualToStudentTwoAge);
+    
     }
 
     public static void main(String[] args) {
         Operators app = new Operators();
-        app.arithmeticOperators();
+        // app.arithmeticOperators();
+        // app.assignmentOperators();
+        app.comparisonOperators();
     }
 }
