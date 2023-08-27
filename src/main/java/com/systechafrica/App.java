4e3fb57e0ddc2eaba2df349a3c1295acec4a743a
Halkano Malise <kudezx@gmail.com>
2023-08-27 17:03:23 +0300

    updated

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -1,60 +1,68 @@
 package com.systechafrica;
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 
 public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
-    //?Additional
+    // ?Additional
     public int add(int a, int b) {
         return a + b;
     }
 
-    //?Subtraction
-    public int sub(int a, int b){
+    // ?Subtraction
+    public int sub(int a, int b) {
         return a - b;
     }
 
-    //?Boolean
-    public boolean positiveNumber(int a){
+    // ?Boolean
+    public boolean positiveNumber(int a) {
         return a >= 0;
     }
 
-    //?Area of a circle
-    public double areOfCircle(double radius){
-        return Math.PI *radius * radius;
+    // ?Area of a circle
+    public double areOfCircle(double radius) {
+        return Math.PI * radius * radius;
     }
 
-    //?division
-    public int divide(int a, int b){
+    // ?division
+    public int divide(int a, int b) {
         return a / b;
     }
 
-    //? if loop
-    public char determiningGrade(int numberGrade){
-        if(numberGrade < 0){
+    // ? if loop
+    public char determiningGrade(int numberGrade) {
+        if (numberGrade < 0) {
             throw new IllegalArgumentException("Number grade cannot be negative");
-        }
-        else if (numberGrade < 60){
+        } else if (numberGrade < 60) {
             return 'F';
-        } 
-        else if (numberGrade < 70){
+        } else if (numberGrade < 70) {
             return 'D';
-        }
-        else if (numberGrade < 80){
+        } else if (numberGrade < 80) {
             return 'C';
-        }
-        else if (numberGrade < 90){
+        } else if (numberGrade < 90) {
             return 'B';
-        }
-        else {
+        } else {
             return 'A';
         }
     }
 
-    //?Next Main Code
-    public static void main( String[] args ){
-        
-        LOGGER.info("Hello World!" );
+    public static void sayName() {
+        LOGGER.info("Say My Name");
+    }
+
+    public void sayAge() {
+        LOGGER.info("My age is " + 27);
+
+    }
+
+    // ?Next Main Code
+    public static void main(String[] args) {
+        App app = new App();
+
+        App.sayName();
+        app.sayAge();
+
+        LOGGER.info("Hello World!");
     }
 }
