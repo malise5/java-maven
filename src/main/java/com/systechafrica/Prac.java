52115831773bad1380b1d870e82ad3f87ee699e8
Halkano Malise <kudezx@gmail.com>
2023-08-29 22:36:50 +0300

    updated

---
+++ src/main/java/com/systechafrica/Prac.java
@@ -0,0 +1,32 @@
+package com.systechafrica;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Prac {
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+  private static void getA() {
+    int[] a = { 1, 2, 3, 4, 5 };
+    for (int i = 0; i < a.length; i += 2) {
+      LOGGER.info("index of " + a[i] + " is " + i);
+    }
+  }
+
+  private static String getName() {
+    Scanner input = new Scanner(System.in);
+    return input.nextLine();
+  }
+
+  private static double getAge() {
+    return new Scanner(System.in).nextDouble();
+  }
+
+  public static void main(String[] args) {
+    // getA();
+    // LOGGER.info("What is Your name ");
+    // LOGGER.info(getName() + " " + getAge());
+
+  }
+
+}
