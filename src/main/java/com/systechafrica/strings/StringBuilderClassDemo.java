6d65ff10fbec3f4002ba20f4a3c51bd020ab7013
Halkano Malise <kudezx@gmail.com>
2023-08-29 07:39:58 +0300

    Strings Builder and Classes Demo

---
+++ src/main/java/com/systechafrica/strings/StringBuilderClassDemo.java
@@ -0,0 +1,19 @@
+package com.systechafrica.strings;
+
+import java.util.logging.Logger;
+
+public class StringBuilderClassDemo {
+  private static final Logger LOGGER = Logger.getLogger(StringBuilderClassDemo.class.getName());
+
+  public static void main(String[] args) {
+
+    StringBuilder sb = new StringBuilder(100); // capacity and not the number of characters
+    sb.append(" ");
+    String str = " ";
+
+    boolean equals = str.equals(sb.toString()); // str == sb.toString(); comparing addresses -> false
+    LOGGER.info("" + equals);
+
+  }
+
+}
---
+++ src/main/java/com/systechafrica/strings/Student.java
@@ -0,0 +1,26 @@
+package com.systechafrica.strings;
+
+public class Student {
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String regNo;
+  private String address;
+  private String nickname;
+
+  public String getFullDetails() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(firstName)
+        .append("")
+        .append(lastName)
+        .append("")
+        .append(email)
+        .append("");
+    return sb.toString();
+  }
+
+  public String getFullDetails2() {
+    return firstName + " " + lastName + " " + email;
+  }
+
+}
