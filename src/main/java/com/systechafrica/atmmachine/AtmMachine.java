64bf236b19bdf56db1730e177f87d77ff2fe83bc
Halkano Malise <kudezx@gmail.com>
2023-08-30 09:35:51 +0300

    created and stored both user_info and balance

--- src/main/java/com/systechafrica/atmmachine/AtmMachine.java
+++ src/main/java/com/systechafrica/atmmachine/AtmMachine.java
@@ -1,5 +1,49 @@
 package com.systechafrica.atmmachine;
 
+import java.util.Scanner;
+
 public class AtmMachine {
+  public static void main(String[] args) {
+    final String DB_USERNAME = "malise";
+    final String DB_PASSWORD = "malise123";
+    int balance = 100_001_000;
+
+    // Scanner scanner = new Scanner(System.in);
+
+    // System.out.println("*******************");
+    // System.out.println("ATM SIMULATOR ");
+    // System.out.println("*******************");
+
+    // System.out.print("Enter your username: ");
+    // String username = scanner.nextLine();
+
+    // System.out.print("Enter your password: ");
+    // String password = scanner.nextLine();
+
+    // if (username.equals(DB_USERNAME) && password.equals(DB_PASSWORD)) {
+    // System.out.println("______________________________________");
+    // System.out.println("Welcome to our SystechBank" + DB_USERNAME);
+    // System.out.println("______________________________________");
+
+    // } else {
+    // System.err.println("Incorrect username or password");
+    // }
+
+    // System.out.println("*******************");
+    // System.out.println("ATM SERVICES ");
+    // System.out.println("*******************");
+
+    // System.out.println("Select an option:");
+    // System.out.println("1. Check Balance");
+    // System.out.println("2. Withdraw");
+    // System.out.println("3. Deposit");
+    // System.out.println("4. Exit");
+
+    // if (scanner.nextInt() == 1) {
+    // System.out.println("Your balance is: $" + balance);
+    // }
+
+    // scanner.close();
+  }
 
 }
