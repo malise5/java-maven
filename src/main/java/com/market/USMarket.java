b4d8f744f3b7bb527b23cedc0c887cc98935d43c
Halkano Malise <kudezx@gmail.com>
2023-09-06 06:53:38 +0300

    packages

---
+++ src/main/java/com/market/USMarket.java
@@ -0,0 +1,10 @@
+package com.market;
+
+import com.sales.SalesMan;
+import com.sales.products.Product;
+
+public class USMarket {
+  SalesMan sm;
+  Product p;
+
+}
---
+++ src/main/java/com/sales/products/Product.java
@@ -0,0 +1,5 @@
+package com.sales.products;
+
+public class Product {
+
+}
