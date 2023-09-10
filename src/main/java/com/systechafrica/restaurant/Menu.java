9504008b4329e9dfe7908bfcc94186612b59dfa5
Halkano Malise <kudezx@gmail.com>
2023-09-10 11:06:49 +0300

    added menu class

---
+++ src/main/java/com/systechafrica/restaurant/Menu.java
@@ -0,0 +1,44 @@
+package com.systechafrica.restaurant;
+
+import java.util.Scanner;
+
+public class Menu {
+
+  public Scanner scanner = new Scanner(System.in);
+  final String DEFAULT_PASSWORD = "Admin123";
+
+  public boolean login() {
+    int attempts = 1;
+    boolean loggedIn = false;
+
+    while (attempts <= 3) {
+      System.out.println("Enter your password: ");
+      String userPassword = scanner.nextLine();
+
+      if (userPassword.equals(DEFAULT_PASSWORD)) {
+        loggedIn = true;
+        break;
+      }
+      System.out.println("Wrong password");
+      attempts++;
+    }
+    return loggedIn;
+
+  }
+
+  public void displayMenu() {
+    System.out.println("-DRINKS------------------------");
+    System.out.println("--------------------------------");
+    System.out.println("1. CHAI-----------------------------------------15");
+    System.out.println("2. ANDAZI---------------------------------------10");
+    System.out.println("2. TOSTI----------------------------------------12");
+
+    System.out.println("-MEALS------------------------");
+    System.out.println("--------------------------------");
+    System.out.println("1. NDENGU AND ACCOMPLISHMENTS--------------------15");
+    System.out.println("2. BEANS AND ACCOMPLISHMENTS---------------------10");
+    System.out.println("2. PILAU VEG-------------------------------------12");
+    System.out.println("3. Quit");
+  }
+
+}
