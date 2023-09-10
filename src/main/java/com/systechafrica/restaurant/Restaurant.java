2ff4957372fd0ee64c22e23eb911b7ab91449e9e
Halkano Malise <kudezx@gmail.com>
2023-09-10 13:59:43 +0300

    added a new feature

--- src/main/java/com/systechafrica/restaurant/Restaurant.java
+++ src/main/java/com/systechafrica/restaurant/Restaurant.java
@@ -9,135 +9,135 @@ public class Restaurant {
   static int initalIndex = 0;
 
   public static void main(String[] args) {
-    // Restaurant restaurant = new Restaurant();
-    // Menu app = new Menu();
-    // boolean loggedIn = app.login();
+    Restaurant restaurant = new Restaurant();
+    Menu app = new Menu();
+    boolean loggedIn = app.login();
 
-    // if (loggedIn) {
-    // boolean keepDisplay = true;
-    // while (keepDisplay) {
-    // app.displayMenu();
-    // System.out.print("Enter Your Meal/Drink Option: ");
-    // int option = restaurant.scanner.nextInt();
-    // restaurant.scanner.nextLine(); // Consume the newline
+    if (loggedIn) {
+      boolean keepDisplay = true;
+      while (keepDisplay) {
+        app.displayMenu();
+        System.out.print("Enter Your Meal/Drink Option: ");
+        int option = restaurant.scanner.nextInt();
+        restaurant.scanner.nextLine(); // Consume the newline
 
-    // switch (option) {
-    // case 1:
-    // menu[initalIndex] = "CHAI";
-    // initialPrice += app.CHAI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem = restaurant.scanner.nextLine();
-    // if (!anotherItem.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+        switch (option) {
+          case 1:
+            menu[initalIndex] = "CHAI";
+            initialPrice += app.CHAI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem = restaurant.scanner.nextLine();
+            if (!anotherItem.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 2:
-    // menu[initalIndex] = "ANDAZI";
-    // initialPrice += app.ANDAZI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem1 = restaurant.scanner.nextLine();
-    // if (!anotherItem1.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+          case 2:
+            menu[initalIndex] = "ANDAZI";
+            initialPrice += app.ANDAZI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem1 = restaurant.scanner.nextLine();
+            if (!anotherItem1.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 3:
-    // menu[0] = "TOSTI";
-    // initialPrice += app.TOSTI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem2 = restaurant.scanner.nextLine();
-    // if (!anotherItem2.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+          case 3:
+            menu[0] = "TOSTI";
+            initialPrice += app.TOSTI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem2 = restaurant.scanner.nextLine();
+            if (!anotherItem2.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 4:
-    // menu[0] = "NDENG AND ACCOMPLISHMENTS";
-    // initialPrice += app.TOSTI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem3 = restaurant.scanner.nextLine();
-    // if (!anotherItem3.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+          case 4:
+            menu[0] = "NDENG AND ACCOMPLISHMENTS";
+            initialPrice += app.TOSTI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem3 = restaurant.scanner.nextLine();
+            if (!anotherItem3.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 5:
-    // menu[0] = "BEANS AND ACCOMPLISHMENTS";
-    // initialPrice += app.TOSTI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem4 = restaurant.scanner.nextLine();
-    // if (!anotherItem4.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+          case 5:
+            menu[0] = "BEANS AND ACCOMPLISHMENTS";
+            initialPrice += app.TOSTI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem4 = restaurant.scanner.nextLine();
+            if (!anotherItem4.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 6:
-    // menu[0] = "BEANS AND ACCOMPLISHMENTS";
-    // initialPrice += app.TOSTI;
-    // System.out.print("Do you want to pick another meal/drink? (y/n): ");
-    // String anotherItem5 = restaurant.scanner.nextLine();
-    // if (!anotherItem5.equals("y")) {
-    // System.out.print("Pay Now For: ");
-    // for (int i = 0; i < menu.length; i++) {
-    // if (menu[i] != null) {
-    // System.out.println(menu[i] + " Price " + initialPrice);
-    // }
-    // }
-    // System.out.println("Total Price: " + initialPrice);
-    // // restaurant.totalDisplay();
-    // keepDisplay = false;
-    // }
-    // break;
+          case 6:
+            menu[0] = "BEANS AND ACCOMPLISHMENTS";
+            initialPrice += app.TOSTI;
+            System.out.print("Do you want to pick another meal/drink? (y/n): ");
+            String anotherItem5 = restaurant.scanner.nextLine();
+            if (!anotherItem5.equals("y")) {
+              System.out.print("Pay Now For: ");
+              for (int i = 0; i < menu.length; i++) {
+                if (menu[i] != null) {
+                  System.out.println(menu[i] + " Price " + initialPrice);
+                }
+              }
+              System.out.println("Total Price: " + initialPrice);
+              // restaurant.totalDisplay();
+              keepDisplay = false;
+            }
+            break;
 
-    // case 7:
-    // System.exit(option);
+          case 7:
+            System.exit(option);
 
-    // default:
-    // System.out.println("Invalid option");
-    // break;
-    // }
-    // }
-    // }
+          default:
+            System.out.println("Invalid option");
+            break;
+        }
+      }
+    }
   }
 }
