2b72e32ae66d6166fef228c5cfb4eddc9169eec1
Halkano Malise <kudezx@gmail.com>
2023-09-19 21:42:30 +0300

    initiated Book class

---
+++ src/main/java/com/systechafrica/part2/interfaces/Book.java
@@ -0,0 +1,39 @@
+package com.systechafrica.part2.interfaces;
+
+public class Book {
+    private String id;
+    private String isbn;
+    private String title;
+    private String authorName;
+
+    public Book(String isbn, String title, String authorName) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authorName = authorName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+}
