ea8dd8f993ece64be0fe47b7f23fcd6b0bad6794
Halkano Malise <kudezx@gmail.com>
2023-10-01 00:20:41 +0300

    added borrow book method

--- src/main/java/com/systechafrica/librarysystem/LibraryDemo.java
+++ src/main/java/com/systechafrica/librarysystem/LibraryDemo.java
@@ -1,6 +1,7 @@
 package com.systechafrica.librarysystem;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LibraryDemo {
@@ -18,14 +19,11 @@ public class LibraryDemo {
     // Create some books and students
     books.add(new Book(1234, "The Lord of the Rings"));
     books.add(new Book(9876, "Harry Potter and the Sorcerer's Stone"));
-
     students.add(new Student(12345));
     students.add(new Student(54321));
   }
 
-  public boolean login() {
-    System.out.println();
-    System.out.println("*********** MAKTABA ***********");
+  private static boolean login() {
     System.out.println();
     System.out.print("Enter Library Admin password to Continue: ");
     String enteredPassword = scanner.nextLine();
@@ -43,6 +41,62 @@ public class LibraryDemo {
     }
 
     return login();
+  }
+
+  private static void displayMenu() {
+    System.out.println("SYSTECH Library Management System::");
+    System.out.println("1. Borrow a Book");
+    System.out.println("2. View borrowed books");
+    System.out.println("3. Return a book");
+    System.out.println("4. Quit");
+    System.out.println("Enter your choice: ");
+  }
+
+  private static void borrowBook() {
+    try {
+      System.out.println("Enter Student registration Number: ");
+      int regNo = Integer.parseInt(scanner.nextLine());
+
+      // Find the student with the given registration number
+      Student student = findStudentRegNo(regNo);
+      if (student == null) {
+        System.out.println("Student not found");
+      }
+
+      // reading the isbn number of the books
+      System.out.println("Enter ISBN number: ");
+      int isbn = Integer.parseInt(scanner.nextLine());
+
+      System.out.println("ENter book title: ");
+      String title = scanner.nextLine();
+
+      // creating the book that is being borrowed
+      Book book = new Book(isbn, title);
+
+      // from the student class we have a method for borrowing the book
+      student.borrowBook(book);
+
+      System.out.println("Do you want to lend another book? (Y/N): ");
+      String answer = scanner.nextLine();
+      if (answer.equalsIgnoreCase("N")) {
+        displayMenu();
+      }
+
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid input. Please enter a valid integer. => " + e.getMessage());
+    }
+  }
+
+  // The findStudentRegNo method is supposed to search through the
+  // list of students and return the Student object that matches the provided
+  // registration number.
+  private static Student findStudentRegNo(int registrationNumber) {
+    for (Student student : students) {
+      if (student.getRegistrationNumber() == registrationNumber) {
+        return student;
+      }
+    }
+    return null;
   }
 
 }
