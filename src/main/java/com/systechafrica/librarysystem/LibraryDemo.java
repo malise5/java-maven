abe0175352823f4fcc97f2610a5464fb8d3a3581
Halkano Malise <kudezx@gmail.com>
2023-09-30 23:12:41 +0300

    Added a login method

--- src/main/java/com/systechafrica/librarysystem/LibraryDemo.java
+++ src/main/java/com/systechafrica/librarysystem/LibraryDemo.java
@@ -1,5 +1,38 @@
 package com.systechafrica.librarysystem;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LibraryDemo {
+
+  private static final String DEFAULT_PASSWORD = "Admin123";
+  private static final int MAX_LOGIN_ATTEMPTS = 3;
+
+  private static Scanner scanner = new Scanner(System.in);
+  private static int loginAttempts = 0;
+
+  private static ArrayList<Student> students = new ArrayList<>();
+
+  public boolean login() {
+    System.out.println();
+    System.out.println("*********** MAKTABA ***********");
+    System.out.println();
+    System.out.print("Enter Admin password to Continue: ");
+    String enteredPassword = scanner.nextLine();
+
+    if (enteredPassword.equals(DEFAULT_PASSWORD)) {
+      return true;
+    }
+
+    loginAttempts++;
+    System.out.println("Incorrect password. Attempts left: " + (MAX_LOGIN_ATTEMPTS - loginAttempts));
+
+    if (loginAttempts >= MAX_LOGIN_ATTEMPTS) {
+      System.out.println("Maximum login attempts reached. Exiting.");
+      return false;
+    }
+
+    return login();
+  }
 
 }
